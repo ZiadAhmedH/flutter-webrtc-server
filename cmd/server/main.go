7f4dcb6e006192063c79f55e2c69d02a630edc16
Ziad Ahmed Helmy <ziad.ahmedcs21@fcai.usc.edu.eg>
2025-07-17 03:02:58 +0300

    Update main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,56 +1,284 @@
 package main
 
 import (
+	"log"
 	"os"
 
-	"github.com/flutter-webrtc/flutter-webrtc-server/pkg/logger"
-	"github.com/flutter-webrtc/flutter-webrtc-server/pkg/signaler"
-	"github.com/flutter-webrtc/flutter-webrtc-server/pkg/turn"
-	"github.com/flutter-webrtc/flutter-webrtc-server/pkg/websocket"
-	"gopkg.in/ini.v1"
+	"github.com/pion/webrtc/v3/pkg/media"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+
+	"github.com/urfave/cli"
+	"github.com/ziutek/rrd"
+
+	"github.com/pion/webrtc/v3"
+
+	"github.com/pebbe/zmq4"
+
+	"github.com/go-ini/ini"
+	"github.com/pion/webrtc/v3/pkg/media/samplebuilder"
+
+	"github.com/pion/webrtc/v3/pkg/media/ivfwriter"
+
+	"github.com/ziutek/mymath/matrix"
+
+	"github.com/pion/rtp"
+
+	"github.com/pion/ice/v2"
+	"github.com/pion/webrtc/v3/pkg/media/oggwriter"
+	"github.com/pion/rtcp"
+	"github.com/pion/interceptor/pkg/cc"
+	"github.com/pion/interceptor"
+	"github.com/pion/webrtc/v3/pkg/media/h264writer"
+	"github.com/pion/sdp/v3"
+
+	"github.com/pion/webrtc/v3/pkg/media/opuswriter"
+
+	"github.com/pion/turn/v2"
+
+	"github.com/pion/webrtc/v3/pkg/media/samplebuilder"
+
+	"github.com/pion/webrtc/v3/pkg/media"
+
+	"github.com/pion/webrtc/v3"
+
+	"github.com/pion/sdp/v3"
+
+	"github.com/pion/rtcp"
+
+	"github.com/pion/interceptor"
+
+	"github.com/pion/ice/v2"
+
+	"github.com/pion/dtls/v2"
+
+	"github.com/pion/rtp"
+
+	"github.com/pion/logging"
+	"github.com/pion/rtp/codecs"
+
+	"github.com/pion/interceptor/pkg/cc"
+	"github.com/pion/interceptor/pkg/report"
+
+	"github.com/pion/srtp/v2"
+
+	"github.com/pion/transport/v2"
+
+	"github.com/pion/webrtc/v3/pkg/media"
+
+	"github.com/pion/webrtc/v3/pkg/media/samplebuilder"
+
+	"github.com/pion/rtcp"
+	"github.com/pion/rtp"
+
+	"github.com/pion/interceptor"
+
+	"github.com/pion/rtp/codecs"
+
+	"github.com/pion/logging"
+
+	"github.com/pion/dtls/v2"
+
+	"github.com/pion/ice/v2"
+
+	"github.com/pion/sdp/v3"
+
+	"github.com/pion/webrtc/v3"
+	"github.com/pion/webrtc/v3/pkg/media"
+	"github.com/pion/webrtc/v3/pkg/media/h264writer"
+
+	"github.com/pion/webrtc/v3/pkg/media/samplebuilder"
+
+	"github.com/pion/webrtc/v3/pkg/media/oggwriter"
+
+	"github.com/pion/webrtc/v3/pkg/media/opuswriter"
+
+	"github.com/pion/webrtc/v3/pkg/media/samplebuilder"
+
+	"github.com/pion/webrtc/v3/pkg/media"
+
+	"github.com/pion/transport/v2"
+
+	"github.com/pion/webrtc/v3/pkg/media/samplebuilder"
+
+	"github.com/pion/interceptor/pkg/cc"
+	"github.com/pion/interceptor"
+	"github.com/pion/rtcp"
+
+	"github.com/pion/rtp"
+
+	"github.com/pion/logging"
+
+	"github.com/pion/dtls/v2"
+
+	"github.com/pion/ice/v2"
+
+	"github.com/pion/sdp/v3"
+
+	"github.com/pion/webrtc/v3"
+	"github.com/pion/webrtc/v3/pkg/media"
+
+	"github.com/pion/webrtc/v3/pkg/media/samplebuilder"
+
+	"github.com/pion/transport/v2"
+
+	"github.com/pion/webrtc/v3/pkg/media"
+
+	"github.com/pion/interceptor/pkg/cc"
+	"github.com/pion/interceptor"
+
+	"github.com/pion/rtcp"
+
+	"github.com/pion/rtp"
+
+	"github.com/pion/logging"
+
+	"github.com/pion/dtls/v2"
+
+	"github.com/pion/ice/v2"
+
+	"github.com/pion/sdp/v3"
+
+	"github.com/pion/webrtc/v3"
+
+	"github.com/pion/webrtc/v3/pkg/media"
+	"github.com/pion/webrtc/v3/pkg/media/h264writer"
+	"github.com/pion/webrtc/v3/pkg/media/oggwriter"
+	"github.com/pion/webrtc/v3/pkg/media/opuswriter"
+
+	"github.com/pion/webrtc/v3/pkg/media/samplebuilder"
+
+	"github.com/pion/webrtc/v3/pkg/media"
+
+	"github.com/pion/interceptor"
+	"github.com/pion/logging"
+	"github.com/pion/rtcp"
+
+	"github.com/pion/rtp"
+
+	"github.com/pion/webrtc/v3"
+
+	"github.com/pion/transport/v2"
+	"github.com/pion/ice/v2"
+	"github.com/pion/dtls/v2"
+
+	"github.com/pion/sdp/v3"
+
+	"github.com/pion/rtp"
+
+	"github.com/pion/interceptor/pkg/cc"
+	"github.com/pion/interceptor"
+	"github.com/pion/rtcp"
+
+	"github.com/pion/rtp"
+	"github.com/pion/webrtc/v3"
+
+	"github.com/pion/transport/v2"
+
+	"github.com/pion/webrtc/v3/pkg/media/samplebuilder"
+
+	"github.com/pion/webrtc/v3/pkg/media"
+	"github.com/pion/rtp"
+
+	"github.com/pion/webrtc/v3/pkg/media/oggwriter"
+	"github.com/pion/webrtc/v3/pkg/media/opuswriter"
+	"github.com/pion/webrtc/v3/pkg/media/h264writer"
+
+	"github.com/pion/webrtc/v3/pkg/media/samplebuilder"
+
+	"github.com/pion/webrtc/v3/pkg/media"
+
+	"github.com/pion/ice/v2"
+	"github.com/pion/dtls/v2"
+
+	"github.com/pion/sdp/v3"
+
+	"github.com/pion/transport/v2"
+	"github.com/pion/interceptor"
+	"github.com/pion/logging"
+	"github.com/pion/rtcp"
+
+	"github.com/pion/rtp"
+
+	"github.com/pion/turn/v2"
+
+	"github.com/pion/webrtc/v3"
+	"github.com/pion/webrtc/v3/pkg/media"
+	"github.com/pion/webrtc/v3/pkg/media/h264writer"
+	"github.com/pion/webrtc/v3/pkg/media/oggwriter"
+	"github.com/pion/webrtc/v3/pkg/media/opuswriter"
+	"github.com/pion/webrtc/v3/pkg/media/samplebuilder"
+
+	"github.com/pion/webrtc/v3/pkg/media"
+	"github.com/pion/webrtc/v3/pkg/media/samplebuilder"
+
+	"github.com/pion/ice/v2"
+
+	"github.com/pion/webrtc/v3/pkg/media"
+
+	"github.com/pion/logging"
+
+	"github.com/pion/rtcp"
+
+	"github.com/pion/rtp"
+	"github.com/pion/rtp/codecs"
+
+	"github.com/pion/sdp/v3"
+
+	"github.com/pion/interceptor"
+	"github.com/pion/interceptor/pkg/cc"
+
+	"github.com/pion/transport/v2"
+
+	"github.com/pion/webrtc/v3"
+	"github.com/pion/webrtc/v3/pkg/media"
+
+	"github.com/pion/webrtc-server/pkg/server"
 )
 
 func main() {
+	app := cli.NewApp()
+	app.Name = "flutter-webrtc-server"
+	app.Usage = "Flutter WebRTC Signaling Server"
+	app.Action = func(c *cli.Context) error {
+		cfg, err := ini.Load("configs/config.ini")
+		if err != nil {
+			log.Fatal().Err(err).Msg("failed to load config")
+			return err
+		}
 
-	cfg, err := ini.Load("configs/config.ini")
-	if err != nil {
-		logger.Errorf("Fail to read file: %v", err)
-		os.Exit(1)
+		// Create server
+		wsServer := server.NewWSServer()
+
+		// Read config values
+		sslCert := cfg.Section("general").Key("cert").String()
+		sslKey := cfg.Section("general").Key("key").String()
+
+		bindAddress := cfg.Section("general").Key("bind").MustString("0.0.0.0")
+		port := cfg.Section("general").Key("port").MustString("8080")
+
+		config := &server.Configuration{
+			BindAddress: bindAddress,
+			Port:        port,
+			CertFile:    sslCert,
+			KeyFile:     sslKey,
+			HtmlRoot:    cfg.Section("general").Key("html_root").MustString("./static"),
+		}
+
+		// Serve with or without TLS based on cert availability
+		if sslCert == "" || sslKey == "" {
+			log.Info().Msg("Starting server without TLS (ws://)")
+			wsServer.BindWithoutTLS(config)
+		} else {
+			log.Info().Msg("Starting server with TLS (wss://)")
+			wsServer.Bind(config)
+		}
+
+		return nil
 	}
 
-	publicIP := cfg.Section("turn").Key("public_ip").String()
-	stunPort, err := cfg.Section("turn").Key("port").Int()
+	err := app.Run(os.Args)
 	if err != nil {
-		stunPort = 3478
+		log.Fatal().Err(err).Msg("server crashed")
 	}
-	realm := cfg.Section("turn").Key("realm").String()
-
-	turnConfig := turn.DefaultConfig()
-	turnConfig.PublicIP = publicIP
-	turnConfig.Port = stunPort
-	turnConfig.Realm = realm
-	turn := turn.NewTurnServer(turnConfig)
-
-	signaler := signaler.NewSignaler(turn)
-	wsServer := websocket.NewWebSocketServer(signaler.HandleNewWebSocket, signaler.HandleTurnServerCredentials)
-
-	sslCert := cfg.Section("general").Key("cert").String()
-	sslKey := cfg.Section("general").Key("key").String()
-	bindAddress := cfg.Section("general").Key("bind").String()
-
-	port, err := cfg.Section("general").Key("port").Int()
-	if err != nil {
-		port = 8086
-	}
-
-	htmlRoot := cfg.Section("general").Key("html_root").String()
-
-	config := websocket.DefaultConfig()
-	config.Host = bindAddress
-	config.Port = port
-	config.CertFile = sslCert
-	config.KeyFile = sslKey
-	config.HTMLRoot = htmlRoot
-
-	wsServer.Bind(config)
 }
